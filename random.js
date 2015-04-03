31727a271b990ec5356a0fc03aa0f15ece6c6454
Juan Quemada <jquemada@dit.upm.es>
2015-04-03 06:17:31 +0200

    random versión 1

---
+++ random.js
@@ -0,0 +1,9 @@
+// Math.random() devuelve número aleatorio entre 0 y 1.
+var numero = Math.random();
+
+if (numero <= 0.5){
+  console.log('\n' + numero + ' MENOR que 0,5 \n');
+}
+else {
+  console.log('\n' + numero + ' MAYOR que 0,5 \n');
+}
